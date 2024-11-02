70d6d44f6083e67be131b78d7ed42bb205764713
Jose Antunes <theholy7@gmail.com>
2024-11-02 12:00:39 -0400

    WIP: Adding fzf search

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.fzf pkgs.vim
+        [ pkgs.vim pkgs.fzf
         ];
 
       # Auto upgrade nix package and the daemon service.
@@ -25,6 +25,11 @@
 
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
+
+      # Enable zsh settings
+      programs.zsh.enableFzfHistory = true;
+      programs.zsh.enableFzfGit = true;
+
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
